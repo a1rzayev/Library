f685bc44330ba8c94065c4773db3ade8c55db821
a1rzayev <asifrzayev2006@example.com>
2025-01-24 21:24:38 +0400

    Add book model

---
+++ models/book.js
@@ -0,0 +1,11 @@
+class Book {
+    constructor(id, title, author, year, genre) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+    }
+}
+
+module.exports = Book;
